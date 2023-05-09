7521cbb5ea09bc0b2e1a1305765bf55117de2b0e
Joey DeLizza <delizza1@go.stockton.edu>
2023-05-09 19:24:23 -0400

    Added Projectile

--- init.el
+++ init.el
@@ -62,3 +62,14 @@
 
 ;; Set directory of backup files
 (setq backup-directory-alist '(("." . "~/.emacs.d/MyEmacsBackups")))
+
+;; Set Evil-normal-mode binding
+(global-set-key (kbd "C-<tab>") 'evil-normal-state)
+
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
